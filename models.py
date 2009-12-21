b30b91e0df3887ff0514815c29f73c7911fcfa0b
Josh Bohde <josh.bohde@gmail.com>
2009-12-20 23:04:03 -0600

    Sample report now generated at /report.
    Started refactoring this out to be more general. Should be able to define individual report sets, and quarters.

--- models.py
+++ models.py
@@ -4,66 +4,90 @@ import datetime
 # Create your models here.
 
 class Program(models.Model):
-	name		=	models.CharField(max_length=64)
-	url			=	models.CharField(max_length=64,null=True,blank=True)
-	def __unicode__(self):
-		return str(self.name)
-		
+    name = models.CharField(max_length=64)
+    url = models.CharField(max_length=64,null=True,blank=True)
+
+    def __unicode__(self):
+        return str(self.name)
+                
 class ProgramBlock(models.Model):
-	start		=	models.TimeField()
-	end			=	models.TimeField()
-	def __unicode__(self):
-		return str(self.start)
-	def slots(self):
-		slots = ProgramSlot.objects.filter(time=self)
-		return slots
-	def current(self):
-		time = datetime.datetime.now()
-		now = datetime.time(hour=time.hour,minute=time.minute)
-		if now >= self.start:
-			if now <= self.end:
-				return True
-		return False
-		
+    start = models.TimeField()
+    end = models.TimeField()
+
+    def __unicode__(self):
+        return str(self.start)
+
+    def slots(self):
+        slots = ProgramSlot.objects.filter(time=self)
+	return slots
+
+    def current(self):
+	time = datetime.datetime.now()
+	now = datetime.time(hour=time.hour,minute=time.minute)
+	if now >= self.start:
+            if now <= self.end:
+                return True
+	    return False
+	    
 class ProgramSlot(models.Model):
-	#blocks for programming
-	
-	#the active toggle is so you don't delete old
-	#blocks with entries tied to them
-	active		=	models.BooleanField()
-	program		=	models.ForeignKey(Program)
-	time		=	models.ForeignKey(ProgramBlock)
+    #blocks for programming
+    #the active toggle is so you don't delete old
+    #blocks with entries tied to them
+    active = models.BooleanField()
+    program = models.ForeignKey(Program)
+    time = models.ForeignKey(ProgramBlock)
 
-	def __unicode__(self):
-		return str(self.time) + " - " + str(self.program)
+    def __unicode__(self):
+        return str(self.time) + " - " + str(self.program)
 
-	def isdone(self):
-		try:
-			e = Entry.objects.filter(date=datetime.datetime.today).get(slot=self)
-			return e
-		except:
-			return False
-				
+    def isdone(self):
+        try:
+            e = Entry.objects.filter(date=datetime.datetime.today).get(slot=self)
+            return e
+        except:
+            return False
+                                
 class Entry(models.Model):
-	#Individual program log entry
-	slot	=	models.ForeignKey(ProgramSlot)
-	date		=	models.DateField(auto_now=True)
-	time		=	models.TimeField(auto_now=True)
-	notes		=	models.CharField(max_length=64)
-	def __unicode__(self):
-		return str(self.time)
-		
+    #Individual program log entry
+    slot = models.ForeignKey(ProgramSlot)
+    date = models.DateField(auto_now=True)
+    time = models.TimeField(auto_now=True)
+    notes = models.CharField(max_length=64)
+    
+    def __unicode__(self):
+        return str(self.time)
+
+
+class Report(models.Model):
+    #Used to generate reports for program director
+    slug = models.SlugField(max_length=20, unique=True)
+    name = models.CharField(max_length=100)
+    program = models.ManyToManyField(Program)
+    
+    def __unicode__(self):
+        return str(self.slug)
+
+class Quarter(models.Model):
+    #Used for defining quaters. This shouldn't change, but what the hell.
+    id = models.SlugField(max_length=3,primary_key=True, unique=True)
+    begin = models.DateField()
+    end = models.DateField()
+
+    def __unicode__(self):
+        return str(self.id)
+
+                
 class DiskJockey(models.Model):
-	user		=	models.OneToOneField(User)
-	name		=	models.CharField(max_length=35)
-	seniority	=	models.IntegerField(default=0)
-	email		=	models.EmailField(max_length=64)
+    user = models.OneToOneField(User)
+    name = models.CharField(max_length=35)
+    seniority = models.IntegerField(default=0)
+    email = models.EmailField(max_length=64)
 
 
 class Semester(models.Model):
-	name		=	models.CharField(max_length=16)
-	start		=	models.DateField()
-	end			=	models.DateField()
+    name = models.CharField(max_length=16)
+    start = models.DateField()
+    end = models.DateField()
 
 DAY_CHOICES = (
   ('Monday', 'Mon'),
@@ -76,21 +100,21 @@ DAY_CHOICES = (
 )
 
 class ShowBlock(models.Model):
-	day			=	models.CharField(max_length=10,choices=DAY_CHOICES)
-	start		=	models.TimeField()
-	end			=	models.TimeField()
-	semester	=	models.ForeignKey(Semester)
+    day = models.CharField(max_length=10,choices=DAY_CHOICES)
+    start = models.TimeField()
+    end = models.TimeField()
+    semester = models.ForeignKey(Semester)
 
 
 class Show(models.Model):
-	name		=	models.CharField(max_length=75)
-	type		=	models.TextField()
-	block		=	models.ForeignKey(ShowBlock,blank=True,null=True)
-	dj			=	models.ForeignKey(DiskJockey)
+    name = models.CharField(max_length=75)
+    type = models.TextField()
+    block = models.ForeignKey(ShowBlock,blank=True,null=True)
+    dj = models.ForeignKey(DiskJockey)
 
 class pick(models.Model):
-	#this will be used for sorting through
-	#show selections
-	block		=	models.ForeignKey(ShowBlock)
-	show		=	models.ForeignKey(Show)
-	rank		=	models.IntegerField()
+    #this will be used for sorting through
+    #show selections
+    block = models.ForeignKey(ShowBlock)
+    show = models.ForeignKey(Show)
+    rank = models.IntegerField()
--- views.py
+++ views.py
@@ -5,32 +5,76 @@ from django import forms
 from kelp.models import *
 from django.forms import ModelForm
 from django.core.paginator import Paginator
-from datetime import datetime
+from datetime import datetime, date, timedelta
 from django.core.urlresolvers import reverse
+from collections import defaultdict
+from itertools import product
+
 
 def index(request):
-	returndata = {'test':'test',}
-	t = loader.get_template('base.html')
-  	c = RequestContext(request,returndata)
-	return HttpResponse(t.render(c))
+    returndata = {'test':'test',}
+    t = loader.get_template('base.html')
+    c = RequestContext(request,returndata)
+    return HttpResponse(t.render(c))
 	
 def showdaily(request):
 
-	blocks = ProgramBlock.objects.all().order_by('start')
+    blocks = ProgramBlock.objects.all().order_by('start')
 	
-	returndata = {'blocks':blocks,}
-	t = loader.get_template('daily.html')
-  	c = RequestContext(request,returndata)
-	return HttpResponse(t.render(c))
+    returndata = {'blocks':blocks,}
+    t = loader.get_template('daily.html')
+    c = RequestContext(request,returndata)
+    return HttpResponse(t.render(c))
 	
 def addentry(request,slot):
 
-	s = ProgramSlot.objects.get(pk=slot)
+    s = ProgramSlot.objects.get(pk=slot)
 	
-	if request.POST:
-		n = request.POST['notes']
-		if n == 'Description':
-			n = ''
+    if request.POST:
+        n = request.POST['notes']
+	if n == 'Description':
+            n = ''
 	
-	e = Entry.objects.create(slot=s,notes=n)
-	return HttpResponseRedirect(reverse("kelp.views.showdaily",))
+    e = Entry.objects.create(slot=s,notes=n)
+    return HttpResponseRedirect(reverse("kelp.views.showdaily",))
+
+def date_generator(begin, end, format):
+    ret = begin
+    delta = timedelta(1)
+    while ret <= end:
+        yield ret.strftime(format)
+        ret += delta
+    return 
+
+def quarterly_report(request, year, quarter):
+    pass
+    
+def gen_report(request):#, year, quarter):
+    begin = (2009,10,1)
+    end = (2009,12,31)
+    
+    date_format = "%d %b %Y"
+    
+    dates = date_generator(date(*begin), date(*end), date_format)
+
+    names = ["60 Second Science", "Earth and Sky"]
+
+    es = Entry.objects.filter(date__gte=datetime(*begin)).filter(date__lte=datetime(*end))
+    es = es.select_related().all()
+    
+    entries = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+
+    for entry in es:
+        entries[entry.date.strftime(date_format)][entry.slot.program.name][entry.notes].append(entry.time)
+
+    def lookup(date, name):
+        if entries.has_key(date) and entries[date].has_key(name):
+            ret_notes = ((note, sorted(time.strftime('%H:%M') for time in times))
+                         for note, times in entries[date][name].iteritems())
+            return (date, name, ret_notes)
+                                 
+        return (date, name, ())
+
+    display_entries = (lookup(a_date, name) for a_date, name in product(dates, names))
+
+    return render_to_response("report.csv", {"entries":display_entries}, mimetype='text/csv')
