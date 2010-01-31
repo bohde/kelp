225c45635747c30ac1ffca6bdadd87c1125d5c00
Josh Bohde <josh.bohde@gmail.com>
2010-01-31 14:05:40 -0600

    Made named url references

--- program_log/urls.py
+++ program_log/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('program_log.views',
   # Search Interface
-  (r'^showschedule$', 'showdaily'),
-  (r'^add/(?P<slot>\d+)$', 'addentry'),
-  (r'^report/(\d{4})/(\w+)/(\w+)$', 'gen_report'),
-  (r'^report$', 'show_reports'),
+  url(r'^showschedule$', 'showdaily', name="log-show-daily"),
+  url(r'^add/(?P<slot>\d+)$', 'addentry', name="log-add-entry"),
+  url(r'^report/(\d{4})/(\w+)/(\w+)$', 'gen_report', name="log-gen-report"),
+  url(r'^report$', 'show_reports', name="log-show-report"),
 )
